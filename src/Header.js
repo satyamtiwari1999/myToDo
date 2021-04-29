8ecaab962a2c89cc707ff862cbe3c22f65268f2f
codetu <satyammtiwarii@gmail.com>
2021-04-29 14:52:54 +0530

    Non Storing Todo App Created

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import './css/header.css'
+
+function Header() {
+    return (
+        <div className="inner-flex">
+            <h1 className="title"> To Do Application </h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Notes.js
@@ -0,0 +1,63 @@
+import { useState } from "react"
+import './css/tasks.css'
+
+function Notes() {
+    // generating list of toDos
+    let [to_do, set_to_do] = useState([
+        {task: "Complete this react app", id: 1},
+        {task: "Study React", id: 2},
+        {task: "Solve DS problems in Js", id: 3}
+    ])
+    // helper function to remove task from the list
+    const handleClickRemoveToDo = (ind) => {
+        set_to_do(to_do.filter((task, i) => {
+            return i !== ind
+        }))
+    }
+
+    // helper function to add task into the list
+    const handleClickAddToDo = (task) => {
+
+        let new_task = {task: task, id : Math.random().toString(36).substr(2, 9)}
+        let x = [...to_do]
+        x.push(new_task)
+        set_to_do(x)
+    }
+
+    // rendering the tasks
+    return (
+        <div className="inner-flex">
+
+                <input className="input-task-field" placeholder="Enter Task" id="new_task"></input>
+
+                <button className="task-add-button" 
+                    onClick={() => {
+                            handleClickAddToDo(document.getElementById("new_task").value)
+                 }}>
+                Add Task
+                </button>
+
+                {
+                    to_do.map((task) => {
+                        return (
+                            <div className="task-container">
+                                <div 
+                                    type="checkbox" 
+                                    onClick={() => {
+                                        handleClickRemoveToDo(to_do.findIndex(curr => {
+                                            return curr === task
+                                        }))
+                                    }}
+                                    key = {task.id}
+                                    className="task-name"
+                                >
+                                        {task.task}
+                                </div>
+                        </div>
+                )})}
+            
+            
+        </div>
+    )}
+
+export default Notes
